e9d93727d0ca4dbf4a10cd8e63fce80738984ce0
a18eliek <a18eliek@student.his.se>
2019-05-08 21:04:05 +0200

    DB-helper med information om vad som ska ske vid skapning av databas, upgradering samt nedgradering

--- app/src/main/java/org/brohede/marcus/sqliteapp/MountainReaderDbHelper.java
+++ app/src/main/java/org/brohede/marcus/sqliteapp/MountainReaderDbHelper.java
@@ -1,14 +1,33 @@
 package org.brohede.marcus.sqliteapp;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by marcus on 2018-04-25.
- */
-
 public class MountainReaderDbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Mountain.db";
 
-    // TODO: You need to add member variables and methods to this helper class
-    // See: https://developer.android.com/training/data-storage/sqlite.html#DbHelper
 
+    public MountainReaderDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(MountainReaderContract.SQL_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(MountainReaderContract.SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
 }
